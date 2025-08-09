8315ad474c3a05bbeb59acd471b7d63277996871
Kayky Matos <kayky7277@gmail.com>
2025-08-09 16:33:16 -0300

    Old front-end project deleted

--- src/SimpleStocker.Web/src/app/models/enums/eSaleStatus.ts
+++
@@ -1,7 +0,0 @@
-export enum ESaleStatus {
-  Pending, // Aguardando pagamento ou confirmação
-  Confirmed, // Venda confirmada e aprovada
-  Shipped, // Produto enviado (se aplicável)
-  Completed, // Venda finalizada com sucesso
-  Cancelled, // Venda cancelada
-}
