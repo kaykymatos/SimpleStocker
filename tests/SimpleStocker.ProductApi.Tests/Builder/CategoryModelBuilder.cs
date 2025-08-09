e37196e730574f5a62165420998863bde0298fa8
Kayky Matos <kayky7277@gmail.com>
2025-08-09 16:29:08 -0300

    Unitary tests refact, now the categorytest is inside of the SimpleStocker.ProductApi.Tests project.

---
+++ tests/SimpleStocker.ProductApi.Tests/Builder/CategoryModelBuilder.cs
@@ -0,0 +1,18 @@
+using FizzWare.NBuilder;
+using SimpleStocker.ProductApi.DTO;
+
+namespace SimpleStocker.ProductApi.Tests.Builder
+{
+    public class CategoryModelBuilder : BuilderBase<CategoryDTO>
+    {
+        protected override void LoadDefault()
+        {
+            _builderInstance = Builder<CategoryDTO>.CreateNew()
+                .With(x => x.Id = 1)
+                .With(x => x.Name = "Categoria Teste")
+                .With(x => x.Description = "Descrição da categoria")
+                .With(x => x.CreatedDate = DateTime.UtcNow)
+                .With(x => x.UpdatedDate = DateTime.UtcNow);
+        }
+    }
+}
